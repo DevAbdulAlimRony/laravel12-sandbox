116a24996a4d7ed6b3ce81ec645a8f3c37d4b6af
Abdul Alim <alim15@cse.pstu.ac.bd>
2026-01-17 14:46:34 +0600

    starter kits

--- app/Helpers/String.php
+++ app/Helpers/String.php
@@ -6,22 +6,15 @@ __('Welcom'); // Return translated string or given value if translation not exis
 class_basename('Foo\Bar\Baz'); // Return the Class name: // Baz
 preg_replace_array('/:[a-z_]+/', ['8:30', '9:00'], 'between :start and :end'); // Output: between 8:30 and 9:00
 
+use Illuminate\Support\Str;
+
 Str::charAt('This', 0); // Returns the character at the specified index. - T
 Str::ascii('û'); // Ascii Value: 'u'
 
 Str::apa('Creating A Project'); // Title case as APA guidline: 'Creating a Project'
 Str::camel('foo_bar'); // 'fooBar'
-
-Str::before('This is my name', 'my name'); // Returns everything before the given value in a string: This is
-Str::after('This is my name', 'This is'); // ' my name'
-Str::afterLast('App\Http\Controllers\Controller', '\\'); // the last occurrence of the given value in a string: 'Controller'
-Str::beforeLast('This is my name is', 'is'); // name
-
-Str::between('This is my name', 'This', 'name'); // ' is my '
-Str::betweenFirst('[a] bc [d]', '[', ']'); // Returns the smallest possible portion of a string between two values: a
-
-Str::chopStart('https://laravel.com', 'https://'); // 'laravel.com': Removes the first occurrence of the given value only if the value appears at the start of the string
-Str::chopEnd('app/Models/Photograph.php', '.php'); // 'app/Models/Photograph': removes the last occurrence of the given value only if the value appears at the end of the string. Can pass array as second argument.
+Str::headline('steve_jobs'); // Steve Jobs
+Str::headline('EmailNotificationSent'); // Email Notification Sent
 
 Str::contains('This is my name', 'my'); // If the given string contains the given value. Output: true. By default case sensitive.
 Str::contains('This is my name', ['my', 'foo']); // If contains anything of the array.
@@ -34,6 +27,28 @@ Str::endsWith('This is my name', 'name'); // true, can pass array
 Str::doesntStartWith('This is my name', ['What', 'That', 'There']);
 Str::doesntEndWith('This is my name', 'dog'); // true
 
+Str::before('This is my name', 'my name'); // Returns everything before the given value in a string: This is
+Str::after('This is my name', 'This is'); // ' my name'
+Str::afterLast('App\Http\Controllers\Controller', '\\'); // the last occurrence of the given value in a string: 'Controller'
+Str::beforeLast('This is my name is', 'is'); // name
+
+Str::between('This is my name', 'This', 'name'); // ' is my '
+Str::betweenFirst('[a] bc [d]', '[', ']'); // Returns the smallest possible portion of a string between two values: a
+
+Str::chopStart('https://laravel.com', 'https://'); // 'laravel.com': Removes the first occurrence of the given value only if the value appears at the start of the string
+Str::chopEnd('app/Models/Photograph.php', '.php'); // 'app/Models/Photograph': removes the last occurrence of the given value only if the value appears at the end of the string. Can pass array as second argument.
+
+$adjusted = Str::finish('this/string', '/'); // Adds a single instance of the given value to a string if it does not already end with that value
+// this/string/. If already contains / at last, it wont add.
+
 Str::deduplicate('The   Laravel   Framework'); // The Laravel Framework
 Str::deduplicate('The---Laravel---Framework', '-'); // The-Laravel-Framework 
 
+// Truncate String
+$excerpt = Str::excerpt('This is my name', 'my', ['radius' => 3]); // Output: '...is my na...' we got three dots before and after and truncate.
+// By default radius is 100
+$excerpt = Str::excerpt('This is my name', 'name', ['radius' => 3, 'omission' => '(...) ']); // '(...) my name'
+
+$decoded = Str::fromBase64('TGFyYXZlbA=='); // Laravel. Decode a base64 to a string.
+
+// Str::inlineMarkdown: GitHub flavored Markdown into inline HTML. Not secured for CORS Attack, add other arguments to make seured. 
