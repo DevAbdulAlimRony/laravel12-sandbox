6f1887af5403d376409aea4ad46876cac36ee038
Abdul Alim <alim15@cse.pstu.ac.bd>
2026-01-17 12:20:35 +0600

    csrf protection

--- app/Helpers/String.php
+++ app/Helpers/String.php
@@ -1,4 +1,39 @@
 <?php
 // Laravel includes a variety of functions for manipulating string values.
 
-echo __('Welcom'); // Return translated string or given value if translation not exist
+echo e('<html>foo</html>'); //  htmlspecialchars function with the double_encode option set to true
+__('Welcom'); // Return translated string or given value if translation not exist
+class_basename('Foo\Bar\Baz'); // Return the Class name: // Baz
+preg_replace_array('/:[a-z_]+/', ['8:30', '9:00'], 'between :start and :end'); // Output: between 8:30 and 9:00
+
+Str::charAt('This', 0); // Returns the character at the specified index. - T
+Str::ascii('û'); // Ascii Value: 'u'
+
+Str::apa('Creating A Project'); // Title case as APA guidline: 'Creating a Project'
+Str::camel('foo_bar'); // 'fooBar'
+
+Str::before('This is my name', 'my name'); // Returns everything before the given value in a string: This is
+Str::after('This is my name', 'This is'); // ' my name'
+Str::afterLast('App\Http\Controllers\Controller', '\\'); // the last occurrence of the given value in a string: 'Controller'
+Str::beforeLast('This is my name is', 'is'); // name
+
+Str::between('This is my name', 'This', 'name'); // ' is my '
+Str::betweenFirst('[a] bc [d]', '[', ']'); // Returns the smallest possible portion of a string between two values: a
+
+Str::chopStart('https://laravel.com', 'https://'); // 'laravel.com': Removes the first occurrence of the given value only if the value appears at the start of the string
+Str::chopEnd('app/Models/Photograph.php', '.php'); // 'app/Models/Photograph': removes the last occurrence of the given value only if the value appears at the end of the string. Can pass array as second argument.
+
+Str::contains('This is my name', 'my'); // If the given string contains the given value. Output: true. By default case sensitive.
+Str::contains('This is my name', ['my', 'foo']); // If contains anything of the array.
+Str::contains('This is my name', 'MY', ignoreCase: true); // Case sensitivity disabled.
+Str::containsAll('This is my name', ['my', 'name']); // If contains all in the array. Can pass ignoreCase.
+Str::doesntContain('This is name', 'my'); // true. Also can pass array or ignoreCase.
+
+Str::startsWith('This is my name', 'This');
+Str::endsWith('This is my name', 'name'); // true, can pass array
+Str::doesntStartWith('This is my name', ['What', 'That', 'There']);
+Str::doesntEndWith('This is my name', 'dog'); // true
+
+Str::deduplicate('The   Laravel   Framework'); // The Laravel Framework
+Str::deduplicate('The---Laravel---Framework', '-'); // The-Laravel-Framework 
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head></head>
+    <head>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
+    <script>
+
+    </script>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         //* Blade: 
         <!-- If we remove .blade from file name, it will render the page. But blade syntaxes will come as plain html
@@ -11,6 +16,8 @@
         <!-- Creating View: php artisan make:view home -->
         <!-- Views separate your controller / application logic from your presentation logic -->
         <!-- Rather than using blade, we can use any starter kits like inertia and vue which use shadcdn/vue and tailwindcss -->
+        <!-- Blade is the simple, yet powerful templating engine that is included with Laravel.  -->
+        <!-- We can use livewire to make our blade modern, reactive and dynamic like vue or react. -->
 
         <!-- Accessing passed data from route or controller: -->
         <!-- Using Raw php -->
@@ -25,7 +32,22 @@
         <h2><?php echo e($name); ?></h2>
          <!-- If we dont want that escaping, use this syntax: -->
          {!! $name !!}
-         <!-- NOw, this is not escaped, if pass html into that variable it will generate html rather than string that is a good candidate for XSS attach -->
+         <!-- Now, this is not escaped, if pass html into that variable it will generate html rather than string that is a good candidate for XSS attach -->
+         <!-- If we dont want double encoding globally in AppServiceprovider's boot: Blade::withoutDoubleEncoding(); -->
+
+         <!-- If we want escape {{}} itself or any directive: Hello, @{{ name }}- output will be Hello {{name}} -->
+         <!-- For directive: @@if(), Output:@if() -->
+
+         <!-- Render an arry as json in order to initialize a JavaScript variable.: -->
+        <?php echo json_encode($array); ?>; <!-- Unescaped which is not good. -->
+        {{ Illuminate\Support\Js::from($array) }}; <!-- Escaped. or you can use Js::from facade -->
+
+        <!-- If we display javascript variable in a large portion of template, wrap into @verbatim -->
+        @verbatim
+            <div class="container">
+                Hello, {{ name }}.
+            </div>
+        @endverbatim
 
         <!-- We can write regular php code in blade syntax also -->
          {{ date('d/m/Y') }}
@@ -39,5 +61,79 @@
          <!-- See app/Views/Composers directory -->
 
          <!-- All Blade Directives: -->
+         <!-- Directives are just shortcuts which provide a very clean, terse way of working with PHP control structures.-->
+         <!-- @if @elseif @else @endif -->
+         <!-- @unless @endunless -->
+         <!-- Records is defined and is not nul: @isset @endisset -->
+         <!-- Records is empty: @empty @endempty -->
+         <!-- @auth @endauth, @guest @endguest. @auth('admin'), @guest('admin') -->
+         <!-- Check if app running in that environment: @production @endproduction, @env('staging') @endenv, @env(['staging', 'production']) -->
+         <!-- If a template inheritance section has content: @hasSection('navigation') @endif,  @sectionMissing('navigation') @endif -->
+         <!-- If a session value exists: @session('status')  @endsession -->
+         <!-- If a context value exists: @context('canonical') @endcontext -->
+         <!-- switch, @case, @break, @default, @endswitch -->
+         <!-- @for ($i = 0; $i < 10; $i++) @endfor -->
+         <!-- Other Loops: @foreach ($users as $user), @forelse ($users as $user) @empty @endforelse, @while (true), @continue, @break -->
+         <!-- With condition continue or break: @continue($user->type == 1), @break($user->number == 5) -->
+         <!-- In foreach loop, we will get a $loop variable: $loop->first, $loop->last
+         If nested loop, access parent's index or first last: $loop->parent->first  -->
+         <!-- Other properties of loop variable: index, iteration, remaining, count, first, last, even, odd, depth, parent. -->
+         
+         <!-- Class Directive to generate class conditionally -->
+         @php
+            $isActive = false;
+            $hasError = true;
+        @endphp
+        <span @class(['p-4', 'font-bold' => $isActive, 'text-gray-500' => ! $isActive, 'bg-red' => $hasError,)></span>
+        <!-- Same goes for @style directory -->
+
+        <!-- @check directive to check if a checkbox is checked:  @checked(old('active', $user->active)) -->
+        <!-- Same goes for @selected -->
+        <!-- Same goes for  @disabled, @required and @readonly -->
+        <!-- @include directive allows you to include a Blade view from within another view. -->
+        <!-- Included view will inherit all data available in the parent view, but we can pass any data: @include('view.name', ['status' => 'complete']) -->
+        <!-- If you attempt to @include a view which does not exist, Laravel will throw an error.  -->
+        <!-- Can use:  @includeIf('view.name', ['status' => 'complete']), dont show error if not present. -->
+        <!-- @includeWhen($boolean, 'view.name', ['status' => 'complete']) -->
+        <!-- @includeUnless($boolean, 'view.name', ['status' => 'complete']) -->
+        <!-- @includeFirst(['custom.admin', 'admin'], ['status' => 'complete']) -->
+        <!-- Combine loops and includes into one line: @each('view.name', $jobs, 'job') -->
+        <!-- @once  directive allows you to define a portion of the template that will only be evaluated once per rendering cycle.  -->
+        <!-- @pushOnce('scripts')  @endPushOnce -->
+
+        <!-- Avoid using the __DIR__ and __FILE__ constants in your Blade views, since they will refer to the location of the cached, compiled view. -->
+
+        <!-- If you only need to use PHP to import a class -->
+        @use('App\Models\Flight')
+        @use('App\Models\{Flight, Airport}')
+        @use(function App\Helpers\format_currency)
+        @use(const App\Constants\MAX_ATTEMPTS)
+        @use('App\Models\Flight', 'FlightModel') <!-- Aliased -->
+
+        <!-- Dependency Injectin: -->
+        <!-- @inject('metrics', 'App\Services\MetricsService') -->
+
+        <!-- Form Handling and Validation -->
+
+        <!-- CSRF Protection -->
+        <!-- Cross-site request forgeries are a type of malicious exploit whereby unauthorized commands are performed on behalf of an authenticated user.  -->
+        <!-- Imagine your application has a /user/email route that accepts a POST request to change the authenticated user's email address. -->
+        <!-- Without CSRF protection, a malicious website could create an HTML form that points to your application's /user/email route and submits the malicious user's own email address -->
+        <!-- To prevent this vulnerability, we need to inspect every incoming POST, PUT, PATCH, or DELETE request for a secret session value that the malicious application is unable to access. -->
+        <!-- Laravel automatically generates a CSRF "token" for each active user session managed by the application. -->
+        <!-- This token is used to verify that the authenticated user is the person actually making the requests to the application.  -->
+        <!-- Anytime you define a "POST", "PUT", "PATCH", or "DELETE" HTML form in your application, you should include a hidden CSRF _token field in the form so that the CSRF protection middleware can validate the request. -->
+        <form method="POST" action="/profile">
+            @csrf
+            <!-- Equivalent to... -->
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+            <!-- ValidateCsrfToken middleware, which is included in the web middleware group by default, will automatically verify that the token in the request input matches the token stored in the session. -->
+        </form>
+        <!-- For SPA application, it can be handled by Sanctum -->
+        <!-- If we want to exclude any url without csrf like Stripr, you should place these kinds of routes outside of the web middleware group or -->
+        <!--add  ....$middleware->validateCsrfTokens(except:.. from documentation in bootstrap/app.php -->
+        
+        <!-- X-CSRF-TOKEN: We can store x-csrf-token in header: <meta name="csrf-token" content="{{ csrf_token() }}"> then can instruct jquery or any library to add the token to all request headers. -->
+        <!-- X-XSRF-TOKEN: By default, the resources/js/bootstrap.js file includes the Axios HTTP library which will automatically send the X-XSRF-TOKEN header for you. -->
     </body>
 </html>
